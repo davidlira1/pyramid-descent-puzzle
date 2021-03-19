cb5b995ccae85d39319d9630115de975adccab44
davidlira1 <liradavid100@gmail.com>
2021-03-18 23:23:41 -0700

    another update on README

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This project is based on a puzzle from Art of Problem Solving [https://artofproblemsolving.com]
 
-To see the React App that uses this puzzle, please visit [https://davidlira1.github.io/pyramid-descent-puzzle]
+To see the React App I built that is based on this puzzle, please visit [https://davidlira1.github.io/pyramid-descent-puzzle]
+
+Enjoy :)
--- src/components/left-panel/left-panel.component.jsx
+++ src/components/left-panel/left-panel.component.jsx
@@ -4,9 +4,9 @@ import './left-panel.styles.scss';
 var LeftPanel = () => (
     <div className="left-panel">
         <h1>Pyramid Descent Puzzle</h1>
-        <p>To solve the puzzle, you must find a path that traverses the pyramid from top to bottom visiting numbers whose product is the given target value.</p>
+        <p>To solve the puzzle, you must find a path that traverses the pyramid from top to bottom visiting numbers whose overall product is the given target value.</p>
         <p>Each step in the path must go down one row, and go either one step to the left or one step to the right.</p>
-        <p>The top box begins your path. Click on a box on the next row to continue.</p>
+        <p>The top box begins your path. Click on a box on the next row to continue, and repeat until you reach the bottom.</p>
     </div>
 )
 
